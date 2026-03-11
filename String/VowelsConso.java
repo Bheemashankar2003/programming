c33521054e6f9f119651a6b2434751130d9d6f9b
Bheemashankar2003 <bheemashankardiwakar324@gmail.com>
2026-03-11 23:24:52 +0530

    daily

--- String/VowelsConso.java
+++ String/VowelsConso.java
@@ -2,4 +2,23 @@ package String;
 
 public class VowelsConso {
     
+    public static void main(String[] args) {
+        
+        String s = "hello World";
+        int vowel = 0;
+        int conso = 0;
+
+        s = s.toLowerCase();
+
+        for(int i = 1;i<s.length();i++){
+            char ch = s.charAt(i);
+        if(ch == 'a' || ch =='e' || ch =='i' || ch =='o' || ch =='u'){
+            vowel++;
+        }else
+          conso++;
+        }
+        System.out.println("Vowels: "+vowel);
+        // System.out.print(" , ");
+        System.out.println("Consonents: "+conso);
+    }
 }

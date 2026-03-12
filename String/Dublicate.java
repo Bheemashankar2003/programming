3dbaaa9d5992f0bd7a3a200a41d7c5bc0747477f
Bheemashankar2003 <bheemashankardiwakar324@gmail.com>
2026-03-12 16:56:31 +0530

    daily

---
+++ String/Dublicate.java
@@ -0,0 +1,19 @@
+package String;
+
+public class Dublicate {
+    
+    public static void main(String[] args) {
+        
+        String s = "programming";
+        String ss = "";
+        
+        for(int i = 0;i<s.length();i++){
+            int count = 0;
+            char ch = s.charAt(i);
+            if(ss.indexOf(ch) == -1){
+                ss+=ch;
+            }
+        }
+        System.out.println(ss);
+    }
+}
---
+++ String/Freq.java
@@ -0,0 +1,13 @@
+package String;
+
+public class Freq {
+    
+    public static void main(String[] args) {
+        
+        String ss = "programming";
+        int freq = 0;
+
+        String str = "";
+        
+    }
+}
